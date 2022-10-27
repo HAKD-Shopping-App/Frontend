d44eeeadab6d3c683a778e9b41d870432a5a60ad
anjanimenikdiwela98 <62288674+anjanimenikdiwela98@users.noreply.github.com>
2022-10-27 17:10:47 +0530

    Create Sign-in, Sign-up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
 import Home from './component/Home';
 import Navbar from './component/Navbar';
 import Products from './component/Products';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Auth from "./Auth"
 
 function App() {
   return (
-    <>
-      <Navbar/>
-      <Home/>
-      <Products/>
-    </>
-    
+    // <>
+    //   <Navbar/>
+    //   <Home/>
+    //   <Products/>s
+    // </>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/">
+      <Route index element={<Home/>}/>
+      <Route path="/auth" element={<Auth />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
