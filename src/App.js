2e98a0f687ecadef52e55c6fe2360dbc12eabb59
anjanimenikdiwela98 <62288674+anjanimenikdiwela98@users.noreply.github.com>
2022-10-27 17:26:34 +0530

    Resolve conflicts

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './component/Home';
 import Products from './component/Products/Products';
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Auth from "./Auth"
+
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
         <Route path="/">
         <Route index element={<Home/>} />
         <Route path="/Products" element={<Products/>} />
+        <Route path="/auth" element={<Auth />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Navbar from './Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Card, Col, Button, Row } from 'react-bootstrap';
+
 
 const Home = () => {
     return (
@@ -7,19 +10,56 @@ const Home = () => {
             <Navbar />
         <div className="hero">
             <div class="card bg-dark text-white border-0">
-                <img src="/assets/bg.jpg" class="card-img" alt="Background" height="600px"/>
+                <img src="/assets/bg.jpg" class="card-img" alt="Background" height="600px" />
                 <div class="card-img-overlay d-flex flex-column justify-content-center">
                     <div className="container">
-                    <h5 class="card-title display-3 fw-bolder mb-0">New Season Arrivals</h5>
-                    <p class="card-text lead fs-2">
-                        Check out all trends
-                    </p>
+                        <h5 class="card-title display-3 fw-bolder mb-0">New Season Arrivals</h5>
+                        <p class="card-text lead fs-2">
+                            Check out all trends
+                        </p>
+                    </div>
+                    <div className="App">
+                        <Container className='p-4'>
+                            <Row>
+                            <Col md="3">
+                                <Card>
+
+                                    <Card.Body>
+                                        <Card.Title style={{ color: "#aa92df",}}>For Customers</Card.Title>
+                                        <Card.Text>
+
+                                        </Card.Text>
+                                        <Button variant="primary"style={{ backgroundColor: "#aa92df", borderStyle: "none", float: "center",}}>
+                                            Shop Now
+                                        </Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col md="3">
+                                <Card>
+
+                                    <Card.Body>
+                                        <Card.Title style={{ color: "#aa92df",}}>For Sellers</Card.Title>
+                                        <Card.Text>
+
+                                        </Card.Text>
+                                        <Button variant="primary"style={{ backgroundColor: "#aa92df", borderStyle: "none", float: "center",}}>
+                                            Advertise
+                                        </Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            </Row>
+                        </Container>
                     </div>
                     
                 </div>
+
             </div>
+
         </div>
         </div>
+
     );
 }
 
