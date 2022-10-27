6bc0bc1dae0c783ac8b2a7ab4010efc908192fc4
anjanimenikdiwela98 <62288674+anjanimenikdiwela98@users.noreply.github.com>
2022-10-27 13:47:24 +0530

    Initial commit

---
+++ src/component/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="hero">
+            <div class="card bg-dark text-white border-0">
+                <img src="/assets/bg.jpg" class="card-img" alt="Background" height="600px"/>
+                <div class="card-img-overlay d-flex flex-column justify-content-center">
+                    <div className="container">
+                    <h5 class="card-title display-3 fw-bolder mb-0">New Season Arrivals</h5>
+                    <p class="card-text lead fs-2">
+                        Check out all trends
+                    </p>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/component/Products.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+const Products = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Products;
